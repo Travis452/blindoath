8bc304e577bc25a5db6b2477bdf53c4624ccedfa
Travis <travisgorski97@gmail.com>
2023-03-30 13:11:04 -0700

    first commit

---
+++ src/products.js
@@ -0,0 +1,11 @@
+import shirt1 from './assets/shirt1.jpg';
+
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Shirt",
+        price: 35.00,
+        productImage: shirt1,
+    }
+];
